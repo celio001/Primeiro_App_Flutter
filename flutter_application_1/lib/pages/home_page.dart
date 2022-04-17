03a2cdd576c78054bc8a638a9eadd31b8763e9d0
Celio <pessoalcelio01@gmail.com>
2022-04-17 17:10:41 -0300

    Primeira versão

---
+++ flutter_application_1/lib/pages/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/item_add_page.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> _list = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          //botão
+          backgroundColor: Colors.green, //Cor do botão
+          child: Icon(Icons.add), //icone do botão
+          onPressed: () async {
+            final result = (await Navigator.push(
+              context,
+              MaterialPageRoute<String>(
+                builder: (context) {
+                  return ItemAddPage();
+                },
+              ),
+            ))!;
+            setState(() {
+              if (result.isNotEmpty) {
+                _list.add(result);
+              }
+            });
+          }), //ação de clicar no botão
+      appBar: AppBar(
+        title: Text('Primeiro App'),
+      ),
+      body: _list.length > 0
+          ? ListView.builder(
+              itemCount: _list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(_list[index]),
+                  ),
+                );
+              },
+            )
+          : Center(
+              child: Text('Não há nenhum Item'),
+            ),
+    );
+  }
+}
---
+++ flutter_application_1/lib/pages/item_add_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ItemAddPage extends StatelessWidget {
+  TextEditingController _controllerText = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        //botão segunda pagina
+        backgroundColor: Colors.green, // cor do botão
+        onPressed: () {
+          Navigator.pop(context,
+              _controllerText.text = _controllerText.text.isEmpty ? '': _controllerText.text ); //para ele voltar para a primeira pagina
+        },
+        child: Icon(Icons.done),
+      ),
+      // ignore: prefer_const_constructors
+      appBar: AppBar(
+        title: Text('Novo Item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Descrição do Item'),
+            TextField(
+              controller: _controllerText,
+            ), //aqui nos temos nosso campo de texto
+          ],
+        ),
+      ),
+    );
+  }
+}
